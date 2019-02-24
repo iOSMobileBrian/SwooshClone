7694821504701d1ecc0fd287b9405f02d8748fb7
Brian Surface <brian.surface@outlook.com>
2019-02-24 14:15:55 -0500

    Lesson 4- added new activity, layout, and intent

--- app/src/main/java/com/swoosh/brian/swooshclone/LeagueActivity.kt
+++ app/src/main/java/com/swoosh/brian/swooshclone/LeagueActivity.kt
@@ -1,12 +1,20 @@
 package com.swoosh.brian.swooshclone
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun leagueNextClick(view:View){
+
+        val skillActivityView = Intent(this, SkillActivity::class.java)
+        startActivity(skillActivityView)
     }
 }
